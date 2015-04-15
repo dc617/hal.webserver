f419214fd7ff52fe1fe500959a0a63bdf6c0e694
veritablyclean <dcole617@gmail.com>
2015-04-14 23:14:32 -0400

    jails added, permissions tweaked

--- makeUser.pl
+++ makeUser.pl
@@ -1,23 +1,19 @@
 #!/usr/bin/perl
 
-use List::MoreUtils 'any';
-
 print "How many users do you want to create?: ";
 chomp($num=<>);
 print "Creating $num users...\n";
+# creates students if it doesn't exist
+system ("groupadd -f students");
 my $students = `members students`; # gets student names
 my @usernames = split / /, $students; # split to array
 for my $i (1..$num){
 	my $username = "group$i";
-	my $matchFound = 0; # set matchFound to false
-	# if username is in usernames, matchFound = true
-	my $matchFound = any {/$username/} @usernames;
-	# flags username and skips
-	if ($matchFound){print "$username already exists\n"; next;}
 	print "User '$username' being created...\n"; 
-	# makes home dir if not existing, creates user, adds to students group
-	system("useradd -g students -m -d /home/$username $username");
-	#creates groups public html directory
+	# makes home dir if not existing, creates user
+	# adds to students group, disable shell access
+	system("useradd -g students -m -d /home/$username -s /sbin/nologin $username");
+	#creates groups public_html directory
 	my $directory = "/home/$username/public_html";
 	unless (-e $directory or mkdir $directory){
 		die "Error creating public_html dir for $username\n";}
@@ -26,9 +22,14 @@ for my $i (1..$num){
 		or die "Error creating index.html for $username\n";
 		print $fh "Hello $username!";
 	close $fh;
+	# give student ownership of public_html dir
+	system("chown $username:students $directory");
+	# chroot jails student to public_html only
+	system("chown root:root /home/$username");
 	# creates starter pass to be changed 'it240username'
 	system("echo $username:it240$username | chpasswd");
 	# forces user to change on first login
 	system("chage -d 0 $username");
+	print "$username created.\n";
 }
 
