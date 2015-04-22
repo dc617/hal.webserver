3554d9a852db72d1209fcdb56d7e74cb1e62beae
veritablyclean <dcole617@gmail.com>
2015-04-22 17:07:10 -0400

    Removed pass, moved to makePass.pl

--- makeUser.pl
+++ makeUser.pl
@@ -3,17 +3,24 @@
 print "How many users do you want to create?: ";
 chomp($num=<>);
 print "Creating $num users...\n";
+
 # creates students if it doesn't exist
 system ("groupadd -f students");
-my $students = `members students`; # gets student names
-my @usernames = split / /, $students; # split to array
+
+# gets student names already created, split to array
+my $students = `members students`;
+my @usernames = split / /, $students;
+
+# makes sure students aren't overwritten
 for my $i (1..$num){
 	my $username = "group$i";
 	print "User '$username' being created...\n"; 
+
 	# makes home dir if not existing, creates user
 	# adds to students group, disable shell access
 	system("useradd -g students -m -d /home/$username -s /sbin/nologin $username");
-	#creates groups public_html directory
+
+	#creates groups public_html directory / index.html
 	my $directory = "/home/$username/public_html";
 	unless (-e $directory or mkdir $directory){
 		die "Error creating public_html dir for $username\n";}
@@ -22,15 +29,13 @@ for my $i (1..$num){
 		or die "Error creating index.html for $username\n";
 		print $fh "Hello $username!";
 	close $fh;
+
 	# give student ownership of public_html dir
 	system("chown $username:students $directory");
+
 	# chroot jails student to public_html only
 	system("chown root:root /home/$username");
-	# creates starter pass to be changed 'it240username'
-	system("echo $username:it240$username | chpasswd");
-	# forces user to change on first login
-# this is causing issues in putty
-#	system("chage -d 0 $username");
+
 	print "$username created.\n";
 }
 
